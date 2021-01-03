b43c8e1e37cc2858e48efdeb11e5c4906d27ba2f
jindw <jindaweihd@163.com>
2021-01-03 14:56:30 +0800

    save egg

--- app/service/user.js
+++ app/service/user.js
@@ -2,7 +2,12 @@ const Service = require('egg').Service
 
 class UserService extends Service {
     async getAll() {
-        return this.ctx.model.User.findAll()
+        const users = await this.ctx.model.User.findAll()
+        return users.map(itm=>({
+            id:itm.id,
+            name:itm.name,
+            age:itm.age,
+        }))
     }
 }
 module.exports = UserService
