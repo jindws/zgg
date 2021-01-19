31627a44e6b22112ad9e7abdbfe5de59f60f16de
jindw <jindawei@youzan.com>
2021-01-19 19:30:57 +0800

    save

--- app/model/user.js
+++ app/model/user.js
@@ -1,7 +1,7 @@
-module.exports = async (app) => {
+module.exports = (app) => {
   const { STRING, DATE, BIGINT } = app.Sequelize;
 
-  if(process.env.test) await this.app.model.sync({ force: true });
+  if(process.env.test)  app.model.sync({ force: true });
 
   return app.model.define("user", {
     realName: STRING(30),
